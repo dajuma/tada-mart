73b20ceca90f1034c982b7ef77d07f5e10a75ad9
dajuma <dajuma1995@gmail.com>
2024-11-28 13:31:19 -0800

    modified index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ProductsView from '../views/ProductsView.vue'
+import ProductdetailsView from '../views/ProductdetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/products',
       name: 'products',
       component: ProductsView,
+    },
+    {
+      path: 'product/:id',
+      name: 'ProductDetails',
+      component: ProductdetailsView,
     },
   ],
 })
