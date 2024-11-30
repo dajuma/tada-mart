b1ebae02b41d019f5c6a97a2efe4200aacd8f96e
dajuma <dajuma1995@gmail.com>
2024-11-29 22:36:18 -0800

    deleted stores.js

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,5 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { createStore } from 'vuex'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
@@ -10,3 +11,28 @@ export const useCounterStore = defineStore('counter', () => {
 
   return { count, doubleCount, increment }
 })
+
+const store = createStore ({
+  state() {
+    return {
+      cart: [],
+    };
+  },
+  mutations: {
+    ADD_TO_CART(state, product) {
+      state.cart.push(product);
+    },
+  },
+  actions: {
+    addToCart({ commit }, product) {
+  commit('ADD_TO_CART'), product;
+},
+ },
+  getters: {
+    cartItems(state) {
+      return state.cart;
+    },
+  },
+});
+
+export default store;
