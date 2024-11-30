2045242e227af7ef5f030fa585f9e59437333a17
dajuma <dajuma1995@gmail.com>
2024-11-29 22:37:28 -0800

    modified main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import store from './stores/stores';
+import store from './stores/counter';
 
 const app = createApp(App)
 
