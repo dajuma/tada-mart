5b2152b26bb9de7808131d5e6047f361248070f7
dajuma <dajuma1995@gmail.com>
2024-11-29 23:01:45 -0800

    deleted counter.js

--- src/stores/Products.js
+++ src/stores/Products.js
@@ -1,16 +1,4 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
 import { createStore } from 'vuex'
-
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
-})
 
 const store = createStore ({
   state() {
