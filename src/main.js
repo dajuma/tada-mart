2f7f75720615a90b73f5ee3e4c8a305630475f33
dajuma <dajuma1995@gmail.com>
2024-12-01 00:20:52 -0800

    modified main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import store from './stores/Products';
+import store from './stores/Products'
 
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
 app.use(store)
-
 app.mount('#app')
