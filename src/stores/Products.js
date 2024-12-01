1ec73b924968d3c29439e278ecff89f0c065da3f
dajuma <dajuma1995@gmail.com>
2024-11-30 21:51:15 -0800

    modified Products.js

--- src/stores/Products.js
+++ src/stores/Products.js
@@ -10,11 +10,17 @@ const store = createStore ({
     ADD_TO_CART(state, product) {
       state.cart.push(product);
     },
+    REMOVE_FROM_CART(state, product) {
+      state.cart = state.cart.filter(item => item !== product);
+    }
   },
   actions: {
     addToCart({ commit }, product) {
-  commit('ADD_TO_CART'), product;
+    commit('ADD_TO_CART', product);
 },
+    removeFromCart({ commit }, product) {
+      commit('REMOVE_FROM_CART', product);
+    }
  },
   getters: {
     cartItems(state) {
