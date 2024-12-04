99538e140733e5b5a43404da833d4a2093805892
dajuma <dajuma1995@gmail.com>
2024-12-04 12:34:57 -0800

    modified HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,68 @@
-<script setup lang="ts">
+<script>
+import Product from "@/components/Product.vue";
+import CategoryBox from "@/components/Category/CategoryBox.vue";
 
+export default {
+  name: "Home",
+  components: {CategoryBox, Product},
+  props: {
+  categories: {
+    type: Array,
+    required: true,
+  },
+  products: {
+    type: Array,
+    required: true,
+  },
+},
+data() {
+    return {
+      categorySize: 0,
+      productSize: 0,
+    };
+  },
+  mounted() {
+  if (this.categories) {
+    this.categorySize = Math.min(6, this.categories.length);
+  }
+  if (this.products) {
+    this.productSize = Math.min(8, this.products.length);
+  }
+},
+};
 </script>
 
 <template>
-  <div class="home">
-    <div class="hero">
-      <div class="hero-background"></div>
-      <h1 class="hero-header">Welcome to tadamart</h1>
+  <div id="home">
+    <div id="background-div" class="hero-background"></div>
+    <!-- Display categories -->
+    <div class="container">
+      <div class="row">
+        <div class="text-center">
+          <h2 class="part-3">Top Categories</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div v-for="index in categorySize" :key="index">
+          <CategoryBox :category="categories[index - 1]" />
+        </div>
+      </div>
+    </div>
+
+    <hr />
+    <!-- Display top products -->
+    <div class="container">
+      <div class="row">
+        <div class="text-center">
+          <h2 class="part-3">Top Products</h2>
+        </div>
+      </div>
+
+      <div class="row">
+        <div v-for="index in productSize" :key="index">
+          <Product :product="products[index - 1]" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
