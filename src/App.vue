e879fc240317d52ec3843dc03def373e831f8978
dajuma <dajuma1995@gmail.com>
2024-12-04 17:35:10 -0800

    modified App.vue

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@ export default defineComponent({
     <RouterLink />
     <FooterSect />
   </div>
+  <CategoryBox :category="category" :products="someProductsArray" />
 </template>
 
 <style scoped>
