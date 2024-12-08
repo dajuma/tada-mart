9d12741499575e01f14120045fb47fef7da75848
dajuma <dajuma1995@gmail.com>
2024-12-08 13:11:16 -0800

    modified HeaderSect.vue'

--- src/components/HeaderSect.vue
+++ src/components/HeaderSect.vue
@@ -4,6 +4,7 @@ export default {
   data() {
     return {
       searchQuery: "",
+      cartItemCount: 3,
     };
   },
   methods: {
@@ -16,6 +17,9 @@ export default {
       } else {
         alert("Please enter a search term.");
       }
+    },
+    toggleSearch() {
+      this.showSearchInput = !this.showSearchInput;
     },
   },
 };
@@ -33,25 +37,36 @@ export default {
     <div class="wrapper">
       <TADAMART msg="Your one-stop motor spare shop" />
 
+      <div class="search-bar">
+       <input
+       type="text"
+        placeholder="Search for products, brands and more..."
+        v-model="searchQuery"
+        @input="handleSearch"
+        class="search-input"
+        />
+      <button class="search-button" @click="performSearch">
+       <img src="/images/search.png" alt="Search" class="search-icon" />
+      </button>
+      </div>
+
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/cartegory">Category</RouterLink>
+        <div class="cart-container">
+        <RouterLink to="/cart" class="cart-link">
+        <img src="/images/shopping-cart.png" alt="Cart" class="cart-icon" />
+        <span v-if="cartItemCount > 0" class="cart-count">{{ cartItemCount }}</span>
+        </RouterLink>
         <RouterLink to="/cart">Cart</RouterLink>
+        </div>
         <RouterLink to="/checkout">Checkout</RouterLink>
         <RouterLink to="/products">Products</RouterLink>
         <RouterLink to="/order-tracking">Order Tracking</RouterLink>
-      </nav>
+       </nav>
 
-      <div class="search-bar">
-        <input
-          type="text"
-          placeholder="Search..."
-          v-model="searchQuery"
-          @input="handleSearch"
-        />
-        <button v-on:click="performSearch">Search</button>
-      </div>
+
       </div>
   </header>
 </template>
@@ -95,28 +110,72 @@ nav a:hover {
   align-items: center;
 }
 .search-bar {
-  position: absolute;
+  position: relative;
   display: flex;
-  align-items: start;
-  justify-content: flex-start;
-  left: 300px;
+  align-items: center;
+  gap: 0;
+  right: 50rem;
 }
-.search-bar input {
-  padding: 0.5rem;
-  font-size: 1rem;
-  border: 1px solid darkred;
-  border-radius: 5px;
-}
-.search-bar button {
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  background-color: #00aaff;
-  color:black;
-  border: none;
-  border-radius: 5px;
+
+.search-icon {
+  width: 2rem;
+  height: 2rem;
   cursor: pointer;
 }
-.search-bar button:hover {
-  background-color: #0077cc;
+
+.search-input {
+  padding: 0.5rem;
+  border-radius: 5%;
 }
+.search-input:hover {
+  background-color: grey;
+}
+
+.search-button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  border-radius: 2%;
+}
+
+.search-button:hover {
+  background-color: grey;
+}
+.cart-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 0;
+  top: 0;
+}
+.cart-link {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+}
+
+.cart-icon {
+  width: 2rem;
+  height: 2rem;
+}
+
+.cart-count {
+  position: absolute;
+  top: -0.25rem;
+  right: -0.25rem;
+  background-color: #e74c3c;
+  color: white;
+  border-radius: 50%;
+  font-size: 0.75rem;
+  padding: 0.1rem 0.4rem;
+  min-width: 1rem;
+  text-align: center;
+  line-height: 1rem;
+  font-weight: bold;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+}
+
+
 </style>
