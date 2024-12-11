f1ad3d682d1c8a710ab205916a35eb911ebfa4d9
dajuma <dajuma1995@gmail.com>
2024-12-10 16:21:06 -0800

    modified index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import AboutView from '../views/AboutView.vue'
 import ProductsView from '../views/ProductsView.vue'
 import ProductdetailsView from '../views/ProductdetailsView.vue'
 import CartView from '@/views/CartView.vue'
+import Login from '@/views/loginView.vue'
+import Register from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +36,14 @@ const router = createRouter({
       name: 'Cart',
       component: CartView,
     },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+  { path: '/register',
+    name: 'Register',
+    component: Register },
   ],
 })
 
