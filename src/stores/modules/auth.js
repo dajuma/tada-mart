59b57ecb40fbb9c0c86b3e984d1fe51666ba2844
magnito2 <magnusotwani@gmail.com>
2024-12-14 16:33:17 +0300

    added api support for auth

--- src/stores/modules/auth.js
+++ src/stores/modules/auth.js
@@ -1,24 +1,129 @@
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
+
+window.axios = axios
+
 export default {
   namespaced: true,
   state: {
     user: null,
   },
   mutations: {
-    setUser(state, user) {
-      state.user = user;
+    SET_USER(state, user) {
+      state.user = user
     },
   },
   actions: {
     registerUser({ commit }, userData) {
-      commit('setUser', userData);
-      console.log('User registered:', userData);
+      // wrap in a promise for view to track status
+      return new Promise((resolve, reject) => {
+        commit('SET_LOADING', true, { root: true}); //loading animation
+
+        // before calling register, set CSRF cookie
+        axios.get(`${import.meta.env.VITE_API_URL}/sanctum/csrf-cookie`).then(() => {
+          // Call register...
+          return axios.post(`${import.meta.env.VITE_API_URL}/register`, userData).then((resp) => {
+            console.log(`response from register `, resp);
+
+            //get user details after registering
+            return axios.get(`${import.meta.env.VITE_API_URL}/api/user`).then(resp => {
+              console.log('User is : ', resp.data);
+              commit('SET_USER', resp.data);
+              commit('SET_LOADING', false, { root: true});
+              resolve('success');
+            });
+          });
+        }).catch(error => {
+          if(error.response){
+
+            // Handle the error in the view
+            reject(error.response.data);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+            reject(error.request);
+          } else {
+          // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+            reject(error.message);
+          }
+          console.log(error.config);
+          commit('SET_LOADING', false, { root: true});
+        });
+      });
     },
+
     loginUser({ commit }, loginData) {
-      commit('setUser', loginData);
-      console.log('User logged in:', loginData);
+
+      //we need to wrap it in a promise for view to detect when action completes
+      return new Promise((resolve, reject) => {
+        commit('SET_LOADING', true, { root: true}); //loading animation
+
+        // before calling login, set CSRF cookie
+        axios.get(`${import.meta.env.VITE_API_URL}/sanctum/csrf-cookie`).then(() => {
+
+          // Login...
+          return axios.post(`${import.meta.env.VITE_API_URL}/login`, loginData).then((resp) => {
+            console.log('response from login ', resp);
+
+            //get user details after login in
+            return axios.get(`${import.meta.env.VITE_API_URL}/api/user`).then(resp => {
+              console.log('User is : ', resp.data);
+              commit('SET_USER', resp.data);
+              commit('SET_LOADING', false, { root: true});
+              resolve('success');
+            });
+
+          }).catch(error => {
+            if(error.response){
+
+              // Handle the error in the view
+              reject(error.response.data);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+              reject(error.request);
+            } else {
+            // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+              reject(error.message);
+            }
+            console.log(error.config);
+            commit('SET_LOADING', false, { root: true});
+            });
+        });
+      });
     },
+
+    checkAuthStatus({ commit }) {
+      return new Promise((resolve, reject) => {
+        // Check if user is authenticated in session, improve to use local storage in future
+        commit('SET_LOADING', true, { root: true});
+        //get user details after registering
+        return axios.get(`${import.meta.env.VITE_API_URL}/api/user`).then(resp => {
+          console.log('User is : ', resp.data);
+          commit('SET_USER', resp.data);
+          commit('SET_LOADING', false, { root: true});
+          resolve('success');
+        }).catch(error => {
+          if(error.response){
+            // Handle the error in the view
+            reject(error.response.data);
+          } else{
+            reject(error.message);
+          }
+        });
+      });
+    }
   },
+
   getters: {
     isAuthenticated: (state) => !!state.user,
   },
-};
+}
