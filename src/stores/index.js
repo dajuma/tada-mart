81fd57a53ae94f8bd7a18fbc960dbe861d06bdc5
magnito2 <magnusotwani@gmail.com>
2024-12-14 16:30:49 +0300

    move loading state to root store

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,6 +3,27 @@ import auth from './modules/auth';
 import products from './modules/products';
 
 const store = createStore ({
+  state() {
+    return {
+      loading: false,
+    }
+  },
+  mutations: {
+    SET_LOADING(state, isLoading) {
+      state.loading = isLoading
+    },
+  },
+  actions: {
+    setLoading({commit}, isLoading){
+      commit('SET_LOADING', isLoading);
+    },
+  },
+  getters: {
+    isLoading(state) {
+      return state.loading
+    },
+  },
+
   modules: {
     auth,
     products
